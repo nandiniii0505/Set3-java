40db8f734f24b82418da3c3f7bd2775e307c703f
nandiniii0505 <eeshamishra555@gmail.com>
2025-12-25 13:05:40 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,16 @@
+class PalindromeNo {
+    public static void main(String[] args) {
+        int n = 24642; int temp = n;
+        int rev = 0;
+      while (n != 0) {
+            int digit = n % 10;
+            rev = rev * 10 + digit;
+            n = n / 10;
+        }
+ if (temp == rev) {
+     System.out.println("Palindrome Number");
+       } else {
+    System.out.println("Not a Palindrome Number");
+        }
+    }
+}
